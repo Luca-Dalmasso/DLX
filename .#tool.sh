5721c6287c29a716e6602c27c8bcd054ae1c4b7f
Luca Dalmasso <ms21.4@localhost.localdomain>
2021-09-19 15:51:39 +0200

    add: synthesis script, post synthesis netlist and report for all pipeline stages

--- .#tool.sh
+++
@@ -1,1 +0,0 @@
-ms21.4@localhost.localdomain.7444:1630289317
--- synthesis.tcl
+++ synthesis.tcl
@@ -1,24 +1,77 @@
-#!/usr/bin/tclsh
+uplevel #0 source /home/ms21.4/Desktop/DLX/DLX_synthesizable/syn/basic_syn
 
-set circuit "DLX"
-
-set report "./${circuit}/reports"
-if {![file exists $report]} {
-	file mkdir $report
-}
-set dirname "./${circuit}/post_syn_net"
-if {![file exists $dirname]} {
-	file mkdir $dirname
+proc mkdir_rep {path} {
+	if {![file exists $path]} {
+		file mkdir $path
+	}
 }
 
-current_design $circuit
-#preliminary compilation on ultra, all flat
+set dir "DLX_post_syn"
+mkdir_rep $dir
+
+create_clock -name "CLK" -period 1 {"CLK"}
+
+#CONTROL UNIT
+set dir "DLX_post_syn/control-unit"
+mkdir_rep $dir
+
+characterize unit_control
+current_design dlx_cu
+set_max_delay 1 -from [all_inputs] -to [all_outputs]
+compile -map_effort high -ungroup_all
+report_power > $dir/power.txt
+report_timing > $dir/timing.txt
+report_area > $dir/area.txt
+
+
+#FETCH UNIT
+set dir "DLX_post_syn/fetch-unit"
+mkdir_rep $dir
+
+current_design "dlx"
+characterize unit_fetch
+current_design FU_N32
+set_max_delay 1 -from [all_inputs] -to [all_outputs]
+compile -map_effort high -ungroup_all
+report_power > $dir/power.txt
+report_timing > $dir/timing.txt
+report_area > $dir/area.txt
+
+#DECODE UNIT
+set dir "DLX_post_syn/decode-unit"
+mkdir_rep $dir
+
+current_design "dlx"
+characterize unit_decode
+current_design DU_N32
+set_max_delay 1 -from [all_inputs] -to [all_outputs]
+compile -map_effort high -ungroup_all
+report_power > $dir/power.txt
+report_timing > $dir/timing.txt
+report_area > $dir/area.txt
+
+#EXECUTION UNIT ULTRA EFFORT
+set dir "DLX_post_syn/execution-unit"
+mkdir_rep $dir
+
+current_design "dlx"
+characterize unit_execution
+current_design EXUNIT_N32
+set_max_delay 1 -from [all_inputs] -to [all_outputs]
 compile_ultra
-report_power > $report/power_report_ULTRA.txt
-report_area > $report/area_report_ULTRA.txt
-report_timing > $report/timing_report_ULTRA.txt
-compile -gate_clock
-report_power > $report/power_report_GATED.txt
-report_area > $report/area_report_GATED.txt
-report_timing > $report/timing_report_GATED.txt
-write -hierarchy -format verilog -output $dirname/dlx.v
+set_dont_touch   EXUNIT_N32
+report_power > $dir/power.txt
+report_timing > $dir/timing.txt
+report_area > $dir/area.txt
+
+#POST SYN NETLIST
+set dir "DLX_post_syn/dlx_post_syn_net"
+mkdir_rep $dir
+
+current_design "dlx"
+set_max_delay 4 -from [all_inputs] -to [all_outputs]
+compile -map_effort high -ungroup_all
+report_power > $dir/power.txt
+report_timing > $dir/timing.txt
+report_area > $dir/area.txt
+write -hierarchy -format verilog -output dlx.v
